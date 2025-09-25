419b30aa9e17d7599a53c3c24222556a42b9aff0
Dev-creator09 <kotapall@ualberta.ca>
2025-09-25 17:36:08 -0600

    Implementing abstract class Shape

--- app/src/main/java/com/example/spider/Shape.java
+++ app/src/main/java/com/example/spider/Shape.java
@@ -1,21 +1,17 @@
 package com.example.spider;
 
-import android.graphics.drawable.shapes.Shape;
+public abstract class Shape {
+    protected int x;
+    protected int y;
 
-public abstract class Circle extends Shape {
-    private int radius;
-
-    public Circle(int x, int y, int radius) {
-        super();
-        this.radius = radius;
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
 }
 
