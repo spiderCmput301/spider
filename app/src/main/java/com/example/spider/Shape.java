293f8cd9fb6cbaf91f6730a9b96bfd4a4b826699
Tanisha Dhull <tdhull@ualberta.ca>
2025-09-26 15:41:40 -0600

    Added Black

--- app/src/main/java/com/example/spider/Shape.java
+++ app/src/main/java/com/example/spider/Shape.java
@@ -2,7 +2,7 @@ package com.example.spider;
 public abstract class Shape {
     protected int x;
     protected int y;
-    private String color = "White";
+    private String color = "Black";
 
     public Shape(int x, int y) {
         this.x = x;
