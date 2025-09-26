4b64b2cbb5f77f88089a779712c328f4b173eb3d
Dev-creator09 <kotapall@ualberta.ca>
2025-09-26 15:52:37 -0600

    Update Circle.java with new changes

--- app/src/main/java/com/example/spider/Circle.java
+++ app/src/main/java/com/example/spider/Circle.java
@@ -1,7 +1,12 @@
 package com.example.spider;
 
 public class Circle extends Shape {
+    private int radius;
     public Circle(int x, int y, int radius) {
+        super(x, y);
         this.radius = radius;
     }
+    public int getRadius() { return radius; }
+    public void setRadius(int r) { this.radius = r; }
 }
+
