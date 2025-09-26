8c7bb896333b6ec909e71d0ea19aa132bcdbc8dc
Dev-creator09 <kotapall@ualberta.ca>
2025-09-26 15:34:04 -0600

    Adding Circle extending shape

--- app/src/main/java/com/example/spider/Circle.java
+++ app/src/main/java/com/example/spider/Circle.java
@@ -1,20 +1,7 @@
 package com.example.spider;
 
-public class Circle extends Shape{
-    int radius;
-
-    // Constructor
+public class Circle extends Shape {
     public Circle(int x, int y, int radius) {
-        super(x, y);       // calls Shape(int x, int y)
-        this.radius = radius;
-    }
-
-    
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setRadius(int radius) {
         this.radius = radius;
     }
 }
